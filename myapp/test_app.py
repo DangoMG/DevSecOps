494b3ba42ff9428207ff34ac9e9b479ba8b665a3
The Pickle Man <121195399+DangoMG@users.noreply.github.com>
2025-03-28 16:38:01 -0700

    Update test_app.py

--- myapp/test_app.py
+++ myapp/test_app.py
@@ -1,12 +1,17 @@
 import unittest
-from app import add, subtract
+from app import app
 
-class TestMath(unittest.TestCase):
+class TestCalcAPI(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
+        response = self.app.get("/add?a=2&b=3")
+        self.assertEqual(response.json['result'], 5)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
+        response = self.app.get("/subtract?a=5&b=3")
+        self.assertEqual(response.json['result'], 2)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
